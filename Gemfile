64fd0137c283977e7fd7fd1ee528b0f6e59c9ea2
Oscar <ochng6654@gmail.com>
2015-10-19 21:17:05 -0700

    lesson 32

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ group :production do
    gem 'rails_12factor'
 end
 gem 'bootstrap-sass'
+gem 'pry-rails'
+gem 'faker'
 group :development do
   gem 'sqlite3'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ require 'faker'
+
+ # Create Posts
+ # 50.times do
+ #   Post.create!(
+ #     title:  Faker::Lorem.sentence,
+ #     body:   Faker::Lorem.paragraph
+ #   )
+ # end
+
+  Post.create!(
+     title:  "TEST POST",
+     body:   "TEST PARAGRAPH"
+   )
+
+
+
+
+ posts = Post.all
+
+ # Create Comments
+ # 100.times do
+ #   Comment.create!(
+ #     post: posts.sample,
+ #     body: Faker::Lorem.paragraph
+ #   )
+ # end
+
+   Comment.create!(
+    post: posts,
+    body: "sample"
+    )
+
+ puts "Seed finished"
+ puts "#{Post.count} posts created"
+ puts "#{Comment.count} comments created"
