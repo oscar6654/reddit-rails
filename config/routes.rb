3395016e0ae7b278a5273cdc1c72eab0a68da48c
Oscar <ochng6654@gmail.com>
2015-10-22 15:28:33 -0700

    Lesson 33

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  get 'welcome/about'
-  root to: 'welcome#contact'
+  resources :posts
+  get 'about' => 'welcome#about'
+  root to: 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,43 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
  require 'faker'
 
- # Create Posts
- # 50.times do
- #   Post.create!(
- #     title:  Faker::Lorem.sentence,
- #     body:   Faker::Lorem.paragraph
- #   )
- # end
-
-  Post.create!(
-     title:  "TEST POST",
-     body:   "TEST PARAGRAPH"
+ #Create Posts
+ 50.times do
+   Post.create!(
+     title:  Faker::Lorem.sentence,
+     body:   Faker::Lorem.paragraph
    )
+ end
+
+  # Post.create!(
+  #    title:  "TEST POST",
+  #    body:   "TEST PARAGRAPH"
+  #  )
 
 
 
 
  posts = Post.all
 
- # Create Comments
- # 100.times do
- #   Comment.create!(
- #     post: posts.sample,
- #     body: Faker::Lorem.paragraph
- #   )
- # end
-
+ #Create Comments
+ 100.times do
    Comment.create!(
-    post: posts,
-    body: "sample"
-    )
+     post: posts.sample,
+     body: Faker::Lorem.paragraph
+   )
+ end
+
+   # Comment.create!(
+   #  post: posts,
+   #  body: "sample"
+   #  )
 
  puts "Seed finished"
  puts "#{Post.count} posts created"
