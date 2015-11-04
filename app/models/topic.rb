de6d64953be54c1964fd374724f48179487b4c0f
Oscar <ochng6654@gmail.com>
2015-11-03 19:40:08 -0800

    Validating osts

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,5 @@
 class Topic < ActiveRecord::Base
   has_many :posts
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 10 }, presence: true
 end
