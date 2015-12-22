2d90c923aafcfe0849ead280ecb9ddddadff4a07
Oscar <ochng6654@gmail.com>
2015-12-22 00:50:54 -0800

    Fixed more bugs

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,31 +1,37 @@
 class VotesController < ApplicationController
-  before_action :load_post_and_vote
+
+before_action :load_post_and_vote
 
   def up_vote
+    # http://apidock.com/rails/ActionController/Base/redirect_to
     update_vote!(1)
     redirect_to :back
   end
 
   def down_vote
+    # http://apidock.com/rails/ActionController/Base/redirect_to
     update_vote!(-1)
     redirect_to :back
   end
 
-  private
-
   def load_post_and_vote
+    # extract into this method the definition of @post and @vote
     @post = Post.find(params[:post_id])
     @vote = @post.votes.where(user_id: current_user.id).first
   end
 
   def update_vote!(new_value)
+    # Extract vote updating/creating logic here.
+    # This method has access to @vote, because of the before_action
     if @vote
       authorize @vote, :update?
-      @vote.update_attributes(:value, new_value)
+      @vote.update_attribute(:value, new_value)
     else
+      #@vote = current_user.votes.create(value: new_value, post: @post)
       @vote = current_user.votes.build(value: new_value, post: @post)
       authorize @vote, :create?
       @vote.save
     end
   end
+
 end
